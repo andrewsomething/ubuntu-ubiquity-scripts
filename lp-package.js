74190eb7a826aa4b1c68e89b9e24b7886e3807eb
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2009-01-15 21:59:11 -0500

    Add lp-package.js

---
+++ lp-package.js
@@ -0,0 +1,23 @@
+/* Launchpad Ubuntu Package Search
+ * Andrew Starr-Bochicchio <andrewsomething@ubuntu.com>
+ * MIT/X11 License  
+ */
+
+CmdUtils.CreateCommand({ 
+  name: "lp-packages",
+  icon: "http://www.ubuntu.com/files/favicon-ubuntu.ico",
+/* homepage: "http://packages.qa.debian.org/common/index.html", */
+  author: { name: "Andrew Starr-Bochicchio", email: "andrewsomething@ubuntu.com"},
+  license: "MIT",
+  description: "Launchpad Ubuntu Package Search",
+  help: "lp-packages [package]",
+  takes: {"package": noun_arb_text},
+  preview: function(pblock, package) {
+    var template = "Search for ${name} on Launchpd";
+    pblock.innerHTML = CmdUtils.renderTemplate(template, {"name": package.text});
+  },
+  execute: function(package) {
+    var url = "https://edge.launchpad.net/ubuntu/+search?text=" + package.text;
+    Utils.openUrlInBrowser(url);
+  }
+});
